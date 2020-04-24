4363cb4ac0d65391ee73888439a7dd74395be1d2
Pedro Henrique Gonzaga <henrique.gonzaga@live.com>
2020-04-24 18:58:07 -0300

    Class Batalha

---
+++ src/br/senac/odissey/batalha/Batalha.java
@@ -0,0 +1,14 @@
+package br.senac.odissey.batalha;
+
+import br.senac.odissey.combatentes.*;
+import java.util.Random;
+
+
+public class Batalha {
+
+private Combatente combatente1;
+private Combatente combatente2;
+private Random rand = new Random();
+
+
+}
